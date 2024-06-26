8133659fe3532d48263291fa5d8a6f90e083c5bd
Eliaquim Jesus Luz <eliaquim.daluz@yahoo.com>
2024-06-26 18:03:38 +0100

    P1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+    padding: 20px;
+`;
+
 function App() {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <>
+            <GlobalStyles />
+            <StyledApp>
+                <row>
+                    <Row type="horizontal">
+                        <Heading as="h1">The Wild Oasis</Heading>
+                        <div>
+                            <Heading as="h2">Check in and Out</Heading>
+                            <Button onClick={() => alert("Check in")}>
+                                Check in
+                            </Button>
+                            <Button variations="secondary">Check out</Button>
+                        </div>
+                    </Row>
+                    <Row>
+                        <Heading as="h3">Form</Heading>
+                        <form>
+                            <Input type="number" placeholder="Check in info." />
+                            <Input
+                                type="number"
+                                placeholder="Check out info."
+                            />
+                        </form>
+                    </Row>
+                </row>
+            </StyledApp>
+        </>
     );
 }
 
