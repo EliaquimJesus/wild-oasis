c0b748443bd617cb59d5445cc97c3178579bbf16
Eliaquim Jesus Luz <eliaquim.daluz@yahoo.com>
2024-06-28 16:12:19 +0100

    P3

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
+import { getCabines } from "../services/apiCabins";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+    useEffect(function () {
+        getCabines().then((data) => console.log(data));
+    }, []);
+    return (
+        <Row type="horizontal">
+            <Heading as="h1">All cabins</Heading>
+            <p>TEST</p>
+            <img
+                src="https://jljndeqsnnuqvmltvcqm.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+                alt="cabin"
+            />
+        </Row>
+    );
 }
 
 export default Cabins;
