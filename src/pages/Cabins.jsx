eefc44a0f6ad5e87535c02c6d21d6a607911cb02
Eliaquim Jesus Luz <eliaquim.daluz@yahoo.com>
2024-07-01 18:04:29 +0100

    P5

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
-  return (
-    <>
-      <Row type="horizontal">
-        <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
-      </Row>
+    const [showForm, setShowForm] = useState(false);
 
-      <Row>
-        <CabinTable />
-      </Row>
-    </>
-  );
+    return (
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">All cabins</Heading>
+                <p>Filter / Sort</p>
+            </Row>
+
+            <Row>
+                <CabinTable />
+                <Button onClick={() => setShowForm((show) => !show)}>
+                    Add new cabin
+                </Button>
+                {showForm && <CreateCabinForm />}
+            </Row>
+        </>
+    );
 }
 
 export default Cabins;
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,22 +1,36 @@
 import supabase from "./supabase";
 
 export async function getCabines() {
-  const { data, error } = await supabase.from("cabins").select("*");
+    const { data, error } = await supabase.from("cabins").select("*");
 
-  if (error) {
-    console.log(error);
-    throw new Error("Cabins could not be loaded");
-  }
-  return data;
+    if (error) {
+        console.log(error);
+        throw new Error("Cabins could not be loaded");
+    }
+    return data;
+}
+
+export async function createCabin(newCabine) {
+    const { data, error } = await supabase
+        .from("cabins")
+        .insert([newCabine])
+        .select();
+
+    if (error) {
+        console.log(error);
+        throw new Error("Cabin could not be created.");
+    }
+
+    return data;
 }
 
 export async function deleteCabin(id) {
-  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+    const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
-  if (error) {
-    console.log(error);
-    throw new Error("Cabin could not be deleted.");
-  }
+    if (error) {
+        console.log(error);
+        throw new Error("Cabin could not be deleted.");
+    }
 
-  return data;
+    return data;
 }
