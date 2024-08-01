4ab7b48d5cbe95fc053a6ff7d9e8a9a4e28fffab
Eliaquim da Luz <eliaquim.daluz@gmail.com>
2024-08-01 21:46:26 +0100

    P25

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,40 +2,51 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
+import SpinnerMini from "../../ui/SpinnerMini";
 import FormRowVertical from "../../ui/FormRowVertical";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("nhekass@example.com");
+  const [password, setPassword] = useState("pass0987");
+  const { login, isPending } = useLogin();
 
-    function handleSubmit() {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <FormRowVertical label="Email address">
-                <Input
-                    type="email"
-                    id="email"
-                    // This makes this form better for password managers
-                    autoComplete="username"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </FormRowVertical>
-            <FormRowVertical label="Password">
-                <Input
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </FormRowVertical>
-            <FormRowVertical>
-                <Button size="large">Login</Button>
-            </FormRowVertical>
-        </Form>
-    );
+    if (!email || !password) return;
+
+    login({ email, password });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormRowVertical label="Email address">
+        <Input
+          type="email"
+          id="email"
+          // This makes this form better for password managers
+          autoComplete="username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={isPending}
+        />
+      </FormRowVertical>
+      <FormRowVertical label="Password">
+        <Input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={isPending}
+        />
+      </FormRowVertical>
+      <FormRowVertical>
+        <Button size="large">{isPending ? <SpinnerMini /> : "Login"}</Button>
+      </FormRowVertical>
+    </Form>
+  );
 }
 
 export default LoginForm;
