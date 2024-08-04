a89e17404e8bfe53ec5aad2dc92450bef773e91d
Eliaquim da Luz <eliaquim.daluz@gmail.com>
2024-08-04 22:15:24 +0100

    P27

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,14 +1,15 @@
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
 import ButtonIcon from "../../ui/ButtonIcon";
 import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function Logout() {
-    const { logout } = useLogout();
-    return (
-        <ButtonIcon onClick={() => logout()}>
-            <HiArrowRightOnRectangle />
-        </ButtonIcon>
-    );
+  const { logout, isPending } = useLogout();
+  return (
+    <ButtonIcon disabled={isPending} onClick={() => logout()}>
+      {!isPending ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
 }
 
 export default Logout;
