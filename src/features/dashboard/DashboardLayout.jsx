6cbaaa86b6c64cb6d45257948bac0e6182064856
Eliaquim Jesus Luz <eliaquim.daluz@yahoo.com>
2024-08-09 16:03:59 +0100

    P35

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,41 +5,40 @@ import { useCabins } from "../../features/cabins/useCabins";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto 34rem auto;
+    gap: 2.4rem;
 `;
 
 function DashboardLayout() {
-  const { bookings, isPending: isPending1 } = useRecentBookings();
-  const {
-    stays,
-    confirmedStays,
-    isPending: isPending2,
-    numDays,
-  } = useRecentStays();
-  const { cabins, isPending: isPending3 } = useCabins();
+    const { bookings, isPending: isPending1 } = useRecentBookings();
+    const {
+        stays,
+        confirmedStays,
+        isPending: isPending2,
+        numDays,
+    } = useRecentStays();
+    const { cabins, isPending: isPending3 } = useCabins();
 
-  if (isPending1 || isPending2 || isPending3) return <Spinner />;
+    if (isPending1 || isPending2 || isPending3) return <Spinner />;
 
-  console.log(confirmedStays);
-
-  return (
-    <StyledDashboardLayout>
-      <Stats
-        bookings={bookings}
-        confirmedStays={stays}
-        numDays={numDays}
-        cabinCount={cabins.length}
-      />
-      <div>Today´s activity</div>
-      <div>Chart stay durations</div>
-      <SalesChart bookings={bookings} numDays={numDays} />
-    </StyledDashboardLayout>
-  );
+    return (
+        <StyledDashboardLayout>
+            <Stats
+                bookings={bookings}
+                confirmedStays={stays}
+                numDays={numDays}
+                cabinCount={cabins.length}
+            />
+            <div>Today´s activity</div>
+            <DurationChart confirmedStays={confirmedStays} />
+            <SalesChart bookings={bookings} numDays={numDays} />
+        </StyledDashboardLayout>
+    );
 }
 
 export default DashboardLayout;
